7a532dc99b6c4087904cd57909d6ffd2d6b1121e
rehmanm <muhammad.abdulrehman@gmail.com>
2023-07-21 15:48:34 -0400

    dashboard page

--- app/(doc)/documentation/page.tsx
+++ app/(doc)/documentation/page.tsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import type {Metadata} from "next";
+import LargeHeading from "@/components/ui/LargeHeading";
+
+export const metadata: Metadata = {
+  title: "Similarity API | Documentation",
+  description: "Free & Open Source text similarity API",
+};
+
 function page() {
-  return <div>page</div>;
+  return (
+    <div className="container max-w-7xl mx-auto mt-12">
+      <div className="flex flex-col items-center gap-6">
+        <LargeHeading>Heading</LargeHeading>
+      </div>
+    </div>
+  );
 }
 
 export default page;
